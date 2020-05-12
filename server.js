ee4105eeb410699afcca9766e0869e08a6acc749
Mauricio Uhlig <mauriciojastrowuhlig@gmail.com>
2020-05-12 22:06:01 +0000

    rename

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
 // index.js
 
-/**
- * Required External Modules
- */
 const express = require("express");
 const path = require("path");
 
-/**
- * App Variables
- */
-
 const app = express();
 const port = process.env.PORT || "3000";
-/**
- *  App Configuration
- */
 
-/**
- * Routes Definitions
- */
+app.use(express.static('public'));
 app.get("/", (req, res) => {
   res.status(200).send("WHATABYTE: Food For Devs");
 });
-/**
- * Server Activation
- */
+
+
 app.listen(port, () => {
   console.log(`Listening to requests on http://weblink:${port}`);
 });
